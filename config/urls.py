1c1dd15262c8224a25e54d6e66d86bf28cee78b4
Risheet Jajoo <rishjajoo18@gmail.com>
2026-09-25 16:30:36 -0400

    initial frontend

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as authorized_view
+from django.contrib.auth import views as auth_views
 from recruiting import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("signup/", views.signup, name="jobs"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="recruiting/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="job_search"), name="logout"),
     path("profile/edit/", views.edit, name="edit_profile"),
     path("seekers/<int:seekers_id>", views.view, name="view_profile"),
     path("", views.job_search, name="job_search"),
